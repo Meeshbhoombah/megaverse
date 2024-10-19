af3efba021ce58f6805e4b8a5ec940761c56ef74
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-10-19 05:16:44 -0400

    Failing POST request

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@
  *
  */
 
+import * as http from 'http';
 import * as https from 'https';
+import * as process from 'process';
 
 const API = 'https://challenge.crossmint.com/api'
 const GOAL = API + `/map/${process.env.CANDIDATE_ID}/goal`;
@@ -27,18 +29,26 @@ https.get(GOAL, (res) => {
         let map = JSON.parse(rawMap).goal;
 
         // The 2D spaces that are creatable by making `POST` requests to 
-        // Crossmint's Megaverse service are 
+        // Crossmint's Megaverse service are numbered starting at 0
         let rowNumber = 0;
         let columnNumber = 0;
 
         for (let row of map) {
-            console.log("Row: ", rowNumber);
 
             for (let column of row) {
-                console.log("Column: ", columnNumber);
-                console.log(column);
+
+                if (column == 'SPACE') {
+                    process.stdout.write('🌌 ');
+                } 
+
+                if (column == 'POLYANET') {
+                    process.stdout.write('🪐 ') 
+                }
+
                 columnNumber++; 
             }
+
+            process.stdout.write('\n');
 
             // Reset column numbering for the next row in the 2D space
             columnNumber = 0;
@@ -46,6 +56,49 @@ https.get(GOAL, (res) => {
             rowNumber++;
         }
 
+        process.stdout.write('\n');
     });
 });
+
+const POST_DATA = JSON.stringify({
+    'row': '2',
+    'column': '2',
+    'candidateId': `process.env.CANDIDATE_ID`
+});
+
+let hostname = `crossmint.challenge.com`;
+
+const options = {
+    hostname,
+    path: `/api/polyanets`,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(POST_DATA),
+    },
+};
+
+console.log(options);
+
+const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+
+    res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+    });
+
+    res.on('end', () => {
+        console.log('No more data in response.');
+    });
+});
+
+req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+});
+
+// Write data to request body
+req.write(POST_DATA);
+req.end();
 
