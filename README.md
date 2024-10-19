0917cd41398eae28d38b1fdd7ace61fe0ea56659
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-10-19 04:11:56 -0400

    Getting and numerating goal map

--- README.md
+++ README.md
@@ -2,7 +2,42 @@
 Mint a Megaverse with Crossmint's Megaverse service
 
 Built for Crossmint's coding challenge, this repository uses the provided 
-Megaverse creator API to build Maps of various "astral objects" (emojis).
+Megaverse creator API to build 2D spaces of various "astral objects" (emojis) by
+making requests to Crossmint's Megaverse service.
+
+## Breakdown
+Initally, we are presented with three types of astral objects:
+- POLYanets 🪐
+- SOLoons 🌙
+- comETHs ☄
+
+We must create a 2D space ourselves by making requests to Crossmint's megaverse
+service API.
+
+### The API
+Available at:
+```
+https://challenge.crossmint.io/api
+```
+
+With Crossmint's Megaverse service (a REST API), we can generate the different 
+astral objects, given that we pass the required `cadidateId` parameter to the
+service. This is provided to an interviewee via email.
+```
+https://challenge.crossmint.io/api?candidate_id={PROVIDED CANDIDATE ID}
+```
+
+### Polyanets
+- `POST` /polyanets
+    + Arguments: row, column
+
+
+### The Challenge
+Broken up into two phases
+
+### Phase One
+The first phase of the coding challenge has us looking to build
+
 
 ## Getting Started
 ### Prequisites
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,49 @@
-const URL = `https://challenge.crossmint.com/api/map/${process.env.CANDIDATE_ID}/goal`;
+/**
+ * src/app.ts
+ *
+ * Makes requests to Crossmint's Megaverse service to build a map based off the 
+ * Goal map provided by Crossmint. 
+ *
+ */
+
+import * as https from 'https';
+
+const API = 'https://challenge.crossmint.com/api'
+const GOAL = API + `/map/${process.env.CANDIDATE_ID}/goal`;
+
+// The Goal map is hidden behind an endpoint that can be fetched with a simple 
+// HTTP `GET` request. This map is simply JSON data that can be parsed into an
+// Matrix of "Rows" and "Columns."
+https.get(GOAL, (res) => {
+    res.on('data', (data) => {
+
+        // Upon inspecting the headers of responses from Crossmint's Megaverse 
+        // service, I found the character set to be `utf-8.` As a result, we can 
+        // simply convert incoming raw data buffers from Crossmint via `utf-8`, 
+        // as is necessary when making `https` requests with `Node.`
+        let rawMap = data.toString('utf8');
+
+        // The Goal map is a raw JSON, hidden behind a key, "goal"
+        let map = JSON.parse(rawMap).goal;
+
+        let rowNumber = 0;
+        let columnNumber = 0;
+
+        for (let row of map) {
+            console.log("Row: ", rowNumber);
+
+            for (let column of row) {
+                console.log("Column: ", columnNumber);
+                console.log(column);
+                columnNumber++; 
+            }
+
+            // Reset column numbering for the next row
+            columnNumber = 0;
+
+            rowNumber++;
+        }
+
+    });
+});
+
